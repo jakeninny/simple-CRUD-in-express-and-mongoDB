634747b9eabfad009c821e636f2c9644d703126d
Jake Ninness <jake.ninness@gmail.com>
2017-07-16 14:51:36 +1200

    create and connect to database.

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, function(){
-  console.log('listening on 3000');
-});
+var db;
+//server
+MongoClient.connect('mongodb://localhost/test-crud', (err, database) => {
+  if (err) return console.log(err);
+  db = database;
+  app.listen(3000, function(){
+    console.log('listening on 3000');
+  });
+})
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 })
 
 app.post('/quotes', (req, res) => {
-  console.log(req.body);
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err);
+
+    console.log('save to database');
+    res.redirect('/');
+  })
 })
