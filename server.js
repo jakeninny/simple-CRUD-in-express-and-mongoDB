02d2312d8eb8490d0079635871c5f9bf849f099e
Jake Ninness <jake.ninness@gmail.com>
2017-07-16 14:06:05 +1200

    Get / read function

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const app = express();
+
+console.log(app);
+
+app.listen(3000, function(){
+  console.log('listening on 3000');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
