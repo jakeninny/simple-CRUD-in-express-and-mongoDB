4aa9e961e1f486c3adeef8c1c71778c268ae1266
Jake Ninness <jake.ninness@gmail.com>
2017-07-16 14:19:33 +1200

    set up post add body parser to handle input

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-console.log(app);
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(3000, function(){
   console.log('listening on 3000');
@@ -9,4 +10,8 @@ app.listen(3000, function(){
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/quotes', (req, res) => {
+  console.log(req.body);
 })
