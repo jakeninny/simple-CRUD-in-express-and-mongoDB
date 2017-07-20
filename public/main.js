fd435c28323d1f3139c26103b1b14d5aea0ea739
Jake Ninness <jake.ninness@gmail.com>
2017-07-20 13:15:42 +1200

    Delete on button click

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 
 var update = document.querySelector('#update');
-
+var del = document.querySelector('#delete');
 
 update.addEventListener('click', function (e) {
   //send PUT request
@@ -11,5 +11,31 @@ update.addEventListener('click', function (e) {
       'name': 'Darth Vader',
       'quote': 'I find your lack of faith disturbing.'
     })
+  }).then( res => {
+    if (res.ok) return res.json()
+  })
+  .then(data => {
+    console.log(data)
+    window.location.reload(true)
+  })
+});
+
+
+del.addEventListener('click', function (e) {
+  fetch('quotes', {
+    method: 'delete',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'name': 'Darth Vader'
+    })
+  })
+  .then( res => {
+    if (res.ok) return res.json()
+  })
+  .then( data => {
+    console.log(data);
+    window.location.redload(true);
   })
 });
--- server.js
+++ server.js
@@ -40,5 +40,27 @@ app.post('/quotes', (req, res) => {
 
 app.put('/quotes', (req, res) => {
   //Handle put request
-  
+  db.collection('quotes').findOneAndUpdate(
+    {
+      name: 'Yoda'
+    },
+    {
+      $set: {
+        name: req.body.name,
+        quote: req.body.quote
+      }
+    },
+    {
+      sort: {_id:-1},
+      upsert: true
+    },
+    (err, result) => {
+      if (err) return res.send(err)
+      res.send(result)
+    }
+  )
+})
+
+app.delete('/quotes', (req, res) => {
+  //handle delete event
 })
